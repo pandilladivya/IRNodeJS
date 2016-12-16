e1d11aa662f978a937b422f1ddcf86972311657e
yvss.santosh <yvss.santosh@gmail.com>
2016-12-16 13:00:26 +0530

    working with mongodb

--- routes/upload.js
+++ routes/upload.js
@@ -3,9 +3,6 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var router = express.Router();
 var mongo = require('mongodb').MongoClient;
-var assert = require('assert');
-
-var qs = require('querystring');
 
 var multer  = require('multer');
 var upload = multer({ dest: 'uploads/' });
@@ -15,43 +12,50 @@ router.use(bodyParser.json());
 
 var URL = "mongodb://localhost:27017/imageStore";
 router.post('/',upload.array('pics'),function(req, res, next){
-
-
-
     var data = {
         ipstr:"something",
         file: req.files
     };
-    console.log(req.files);
-
-
     mongo.connect(URL,function(err,db){
-        assert.equal(err,null);
-        db.collection('file_data').insertOne(data,function(err,result){
-            assert.equal(err,null);
-            console.log('Data inserted');
-            res.send('Data inserted');
-            db.close();
-        });
+        if(err)
+        {
+            res.json("Error",err);
+        }else{
+            db.collection('file_data').insertOne(data,function(err,result){
+                if(err)
+                {
+                    res.json("Error",err);
+                }else {
+                    res.json(data);
+                    db.close();
+                }
+            });
+        }
+
     });
 
 });
 
-
-
 router.get('/',function(req,res,next){
     var resultArray = [];
-
     mongo.connect(URL,function(err,db){
-        assert.equal(err,null);
-        var cursor = db.collection('file_data').find();
-        cursor.forEach(function(document,err){
-            assert.equal(err,null);
-            resultArray.push(document);
-        },function(){
-            db.close();
-            res.render('upload',{items: resultArray});
-        });
+        if(err)
+        {
+            res.json("Error",err);
+        }else {
+            var cursor = db.collection('file_data').find();
+            cursor.forEach(function (document, err) {
+                if(err)
+                {
+                    res.json("Error",err);
+                }else {
+                    resultArray.push(document);
+                }
+            }, function () {
+                db.close();
+                res.json(resultArray);
+            });
+        }
     });
 });
 
